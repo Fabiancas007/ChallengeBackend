ba294ed67f322b364954252dc8d1682e31374f83
Fabián Católico <83664068+Fabiancas007@users.noreply.github.com>
2025-01-20 22:21:14 -0500

    Primer commit

---
+++ src/main/java/com/alura/foro/ChallengeBackend.java
@@ -0,0 +1,13 @@
+package com.alura.foro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackend.class, args);
+	}
+
+}
